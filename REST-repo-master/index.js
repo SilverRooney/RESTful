a8a0907d78bf32d1ea523191669207dd71fe2b61
silver <silver.rune.dev@gmail.com>
2022-03-11 20:36:27 -0600

    first commit

---
+++ REST-repo-master/index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const app = express();
+const methodOverride = require('method-override')
+const path = require('path');
+const { v4: uuid } = require('uuid');
+ 
+
+
+app.use(express.urlencoded({ extended: true })) 
+app.use(express.json())
+app.use(methodOverride('_method'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+let comments = [
+    {  
+        id: uuid(),
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {   
+        id: uuid(),
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {   
+        id: uuid(),
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {   
+       id: uuid(),
+       username: 'onlysaywoof',
+       comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('commentsindex', { comments })
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('commentsnew')
+})
+
+app.post('/comments', (req, res) => { //create route
+    const {username, comment} = req.body;
+    comments.push({ username, comment, id: uuid(), })
+    res.redirect('/comments')
+})  
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params
+    const comment = comments.find( c => c.id === id)
+    res.render('commentsshow', { comment })
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params
+    const comment = comments.find( c => c.id === id)
+    res.render('commentsedit', { comment })
+})
+
+app.patch('/comments/:id', (req, res) => {  //patch route
+    const { id } = req.params
+    const newCommentText = req.body.comment
+    const foundComment = comments.find( c => c.id === id)
+    foundComment.comment = newCommentText
+    res.redirect('/comments')
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params
+    comments = comments.filter(c => c.id !== id)
+    res.redirect('/comments')
+})
+
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response")
+})
+
+app.post('/tacos', (req, res) => {
+    const {meat, qty} = req.body;
+    res.send(`Ok, here are your ${qty} ${meat} tacos`)
+})
+
+app.listen(3000, () => {
+    console.log("ON PORT 3000")
+})
+
+
+
